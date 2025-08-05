9a043d91e099571d7be24499f160ededb3c89682
Alex Saju <alexsajued@gmail.com>
2025-08-05 13:55:02 +0530

    Update book.java

--- book.java
+++ book.java
@@ -1,1 +1,33 @@
-.
+class Book {
+    
+    private String title;
+    private String author;
+    private double price;
+
+   
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book information
+    public void displayBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+        System.out.println("----------------------");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 12.50);
+
+        
+        book1.displayBookInfo();
+        book2.displayBookInfo();
+    }
+}
