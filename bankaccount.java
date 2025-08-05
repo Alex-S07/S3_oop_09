dcfe9c0d8ef4434a8de1065a38ba63c4e50e6b52
Alex Saju <alexsajued@gmail.com>
2025-08-05 13:57:15 +0530

    Update bankaccount.java

--- bankaccount.java
+++ bankaccount.java
@@ -1,1 +1,33 @@
-.
+class BankAccount {
+  
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+   
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+   
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+   
+    public void displayDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: " + balance);
+        System.out.println("---------------------");
+    }
+}
+public class Main {
+    public static
