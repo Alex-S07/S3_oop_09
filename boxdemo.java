8f5e57e8d72c561ea9b763b9bb6431a7360dcc43
Alex Saju <alexsajued@gmail.com>
2025-08-08 13:37:54 +0530

    Update boxdemo.java

--- boxdemo.java
+++ boxdemo.java
@@ -1,39 +1,31 @@
+package module2;
 class Box {
     double width, height, depth;
-
-    // Constructor with three parameters
     Box(double w, double h, double d) {
         width = w;
         height = h;
         depth = d;
     }
-
-    // Default constructor
     Box() {
         width = -1;
         height = -1;
         depth = -1;
     }
-
-    // Constructor with one parameter
     Box(double len) {
         width = height = depth = len;
     }
-
-    // Method to calculate volume
     void vol() {
         System.out.println("The volume = " + (height * width * depth));
     }
 }
-class BoxDemo3 {
+class BoxDemo {
     public static void main(String[] args) {
         Box b = new Box();
         b.vol();
-
         Box b1 = new Box(5, 6, 4);
         b1.vol();
-
         Box b2 = new Box(5);
         b2.vol();
     }
 }
+
